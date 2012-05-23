6985b62590971eb47ee2add7910b2dda7e72a133
Rob Abby <robabby23@gmail.com>
2012-05-23 11:57:44 -0500

    General Update
    Swapped content from index.php & about.php.  Added resources to
    blank.php.

--- index.php
+++ index.php
@@ -21,11 +21,36 @@
 
 <!-- #### MAIN CONTENT GOES HERE #### -->
 
-<div class="container">
-  <div class="hero-unit">
-    
-  </div>
-</div><!-- .container -->
+<div role="main" id="container" class="container">
+  <div class="row">
+    <div class="span12">
+      <div class="hero-unit">
+        <h1>Web Development Boilerplate</h1>
+        <h2>Everything you will ever need to build amazing websites!</h2>
+      </div><!-- .hero-unit -->
+    </div><!-- .span12 -->
+  </div><!-- .row -->
+  <hr />
+  <div class="row">
+    <div class="span5">
+      <h3>Welcome</h3>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </div><!-- .span6 -->
+    <div class="span5 offset2 textalign-right">
+      <h3>So...what?</h3>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </div><!-- .span6 -->
+  </div><!-- .row -->
+  <hr />
+  <br />
+  <div class="row">
+    <div class="span12">
+        <h3>Headine</h3>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </div><!-- .span12 -->
+  </div><!-- .row -->
+  <hr />
+</div><!-- #container -->
 
 <!-- #### MAIN CONTENT GOES HERE #### -->
 
