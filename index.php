923ce7a45a552438a960c9986160babd5541d536
Rob Abby <robabby23@gmail.com>
2012-05-23 13:02:36 -0500

    Added Content to Home Page

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
 <html>
 <head>
   <?php include('./assets/inc/head.inc.php'); ?>
+  <style>
+  a {color:#06E;}
+  </style>
 </head>
 <body id="home">
 <!-- ## CONTACT MODAL ## -->
@@ -34,19 +37,23 @@
   <div class="row">
     <div class="span5">
       <h3>Welcome</h3>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <p>This is a boilerplate consisting of everything I have ever wanted or needed when building a modern website.  Consisting of fused frameworks, custom fonts, PHP mailer scripts, oh, and a starter Sencha Touch app.  Essentially just about everything you would ever want to fastrack the start of your next project!</p>
     </div><!-- .span6 -->
     <div class="span5 offset2 textalign-right">
       <h3>So...what?</h3>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <p>More often than not, when I go to 'sketch' out an idea in code, I find that getting my environement and workbench set up would take longer than actually getting my idea out of my head.  By the time I had my HTML sheet built and was ready to start coding, I was exhausted from the set-up process.</p>
     </div><!-- .span6 -->
   </div><!-- .row -->
   <hr />
-  <br />
   <div class="row">
     <div class="span12">
-        <h3>Headine</h3>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <h3>Why use this boilerplate?</h3>
+      <p>If you are like me, then you like to go from concept to production quickly.  Nothing is more demotivating to me to get a concept built, then have to style and position elements.  It's so much work!  I love to rapidly develop my concepts in a custom environment that not only looks good, but has everything I need to get to work quickly and easily.  Well with this boilerplate, I have enabled anyone familiar with the code-base to do just that.</p>
+      <hr />
+      <h3>What does it have?</h3>
+      <p>Given the nature of what I enjoy working with, this boilerplate is a bit more advanced than your average HTML/CSS boilerplate.  I started with <a href="#" title="">HTML5 Boilerplate</a>, and converted the standard style.css to a reset.css.  From there I added in <a href="#" title="">Twitter Bootstrap</a>,  offering me all of the rapid development tools I may want to draft quick concepts.  As well as a CSS grid framework and JavaScript plugins. A quick inspection of the files will show you that I have turned as many portions of the code into PHP includes as I could, thus reducing the amount of code-bloat present in the HTML documents and making them simpler and easier to navigate.</p>
+
+      <p>I have also added in my own custom style.css which I use to overwrite some of the standard styles apparent in Bootstrap, and giving the site a more custom look and feel than the overworked Bootstrap theme.  From there I added in a nice selection of custom fonts. You can view a full list of what is available over on <a href="#" title="">GitHub</a>, as well as form the respository for yourself.</p>
     </div><!-- .span12 -->
   </div><!-- .row -->
   <hr />
