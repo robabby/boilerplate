b02deae969a66f06b419a8da026201ca7c51edd6
Rob Abby <robabby23@gmail.com>
2012-05-24 14:02:03 -0500

    Added .active class to Navigation

--- assets/inc/nav.inc.php
+++ assets/inc/nav.inc.php
@@ -4,9 +4,12 @@
     <nav>
       <ul>
         <li><a href="index.php">Home</a></li>
-        <li><a href="about.php">About</a></li>
-        <li><a href="sandbox.php">Sandbox</a></li>
-        <li><a href="blog.php">Blog</a></li>
+        <li <?php if ($currentPage == 'about.php') {
+          echo 'class="active"';} ?>><a href="about.php">About</a></li>
+        <li <?php if ($currentPage == 'sandbox.php') {
+          echo 'class="active"';} ?>><a href="sandbox.php">Sandbox</a></li>
+        <li <?php if ($currentPage == 'blog.php') {
+          echo 'class="active"';} ?>><a href="blog.php">Blog</a></li>
         <li class="last"><a href="contact.php">Contact</a></li>
       </ul>
     </nav>
