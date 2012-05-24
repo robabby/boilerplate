600ac7f26a3e1df4fdd41cb53baec9ff6648e834
Rob Abby <robabby23@gmail.com>
2012-05-24 13:38:25 -0500

    Modified drag_drop.php to accept JPEG & PNG

--- drag_drop.php
+++ drag_drop.php
@@ -189,8 +189,8 @@ pre {
 
     Output(
       "<p>File information: <strong>" + file.name +
-      "</strong> type: <strong>" + file.type +
-      "</strong> size: <strong>" + file.size +
+      "</strong><br /> type: <strong>" + file.type +
+      "</strong><br /> size: <strong>" + file.size +
       "</strong> bytes</p>"
     );
 
@@ -224,40 +224,7 @@ pre {
   function UploadFile(file) {
 
     var xhr = new XMLHttpRequest();
-    if (xhr.upload && file.type == "image/jpeg" && file.size <= $id("MAX_FILE_SIZE").value) {
-
-      // create progress bar
-      var o = $id("progress");
-      var progress = o.appendChild(document.createElement("p"));
-      progress.appendChild(document.createTextNode("upload " + file.name));
-
-
-      // progress bar
-      xhr.upload.addEventListener("progress", function(e) {
-        var pc = parseInt(100 - (e.loaded / e.total * 100));
-        progress.style.backgroundPosition = pc + "% 0";
-      }, false);
-
-      // file received/failed
-      xhr.onreadystatechange = function(e) {
-        if (xhr.readyState == 4) {
-          progress.className = (xhr.status == 200 ? "success" : "failure");
-        }
-      };
-
-      // start upload
-      xhr.open("POST", $id("upload").action, true);
-      xhr.setRequestHeader("X_FILENAME", file.name);
-      xhr.send(file);
-
-    }
-  }
-  
-  // upload PNG files
-  function UploadFile(file) {
-
-    var xhr = new XMLHttpRequest();
-    if (xhr.upload && file.type == "image/png" && file.size <= $id("MAX_FILE_SIZE").value) {
+    if (xhr.upload && file.type == "image/jpeg" || "image/png" && file.size <= $id("MAX_FILE_SIZE").value) {
 
       // create progress bar
       var o = $id("progress");
